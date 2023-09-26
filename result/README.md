130beb6df0a6393dc6b69ad3c5f3a2d91a1d04e4
Lenzlee Wheeler <lwheeler@luc.edu>
2023-09-26 01:08:38 -0500

    updated files

---
+++ result/README.md
@@ -0,0 +1,31 @@
+# ReadMe File Generator
+        
+        ## Description
+        This app will auto generate a readme file based on the user's answers to a series of questions.
+        
+        ## Table of Contents
+        *[Installation](#installation)
+        *[Usage](#usage)
+        *[License](#license)
+        *[Contributing](#contributing)
+        *[Tests](#tests)
+        *[Questions](#questions)
+        
+        ## Installation
+        To install dependencies, run the command: npm i
+        
+        ## Usage
+        description of how the user can use the repo goes here
+        
+        ## License
+        This repository is licensed under the MIT license.
+        
+        ## Contributing
+        instructions to contribute go here
+        
+        ## Tests
+        To run tests, run the command: npm run tests
+        
+        ## Questions
+        If you have any questions about this repository, please contact [lenzlee@gmail.com](mailto:lenzlee@gmail.com).
+        To view more of my projects in GitHub, visit [lenzlee](https://github.com/lenzlee)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,59 +1,41 @@
-// TODO: Create a function that returns a license badge and the license link based on which license is passed in; If there is no license, return an empty string
+// TODO: Create a function that returns the license link based on which license is passed in; If there is no license, return an empty string
 
 const licenseBadgeLinks = require("./licenseBadges");
     
     // TODO: Create a function to generate markdown for README
     function generateMarkdown(data) {
-      
-      data.licenseBadge = licenseBadgeLinks[data.license];
-    
-      return `# ${data.title}
-      ${data.licenseBadge}
-    
-      ## Description
-      ${data.description}
-    
-      ## Table of Contents
-      * [Installation](#installation)
-      * [Usage](#usage)
-      * [License](#license)
-      * [Contributing](#contributing)
-      * [Tests](#tests)
-      * [Questions](#questions)
-      
-      ## Installation
-    
-    To install dependencies, run the following:
-    
-    \`
-    ${data.installation}
-    \`
-    
-    ## Usage
-    
-    ${data.usage}
-    
-    ## License
-    
-    This repository is licensed under the ${data.license} license.
-    
-    ## Contributing
-    
-    ${data.contribute}
-    
-    ## Tests
-    
-    To run tests, run the following:
-    
-    \`
-    ${data.tests}
-    \`
-    
-    ## Questions
-    
-    Questions about this repository? Please contact me at [${data.email}](mailto:${data.email}). View more of my work in GitHub at [${data.username}](https://github.com/${data.username}) 
-    
-    `;
+        
+        return `# ${data.title}
+        
+        ## Description
+        ${data.description}
+        
+        ## Table of Contents
+        *[Installation](#installation)
+        *[Usage](#usage)
+        *[License](#license)
+        *[Contributing](#contributing)
+        *[Tests](#tests)
+        *[Questions](#questions)
+        
+        ## Installation
+        To install dependencies, run the command: ${data.installation}
+        
+        ## Usage
+        ${data.usage}
+        
+        ## License
+        This repository is licensed under the ${data.license} license.
+        
+        ## Contributing
+        ${data.contribute}
+        
+        ## Tests
+        To run tests, run the command: ${data.tests}
+        
+        ## Questions
+        If you have any questions about this repository, please contact [${data.email}](mailto:${data.email}).
+        To view more of my projects in GitHub, visit [${data.username}](https://github.com/${data.username})`;
     }
     
     module.exports = generateMarkdown;
